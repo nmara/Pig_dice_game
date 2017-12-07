e311ecbf9f1a701135a9a650505be9b9b5a0ef78
nmara <aga.czekala89@gmail.com>
2017-12-08 00:42:01 +0100

    changing players

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var scores, roundScore, activePlayer;
 
 scores = [0,0];
 roundScore = 0;
-activePlayer = 1;
+activePlayer = 0;
 
 document.querySelector(".dice").style.display = "none"; //Nie potrzebujemy kostki na początku
 document.getElementById("score-0").textContent = "0";
@@ -21,8 +21,37 @@ document.querySelector(".btn-roll").addEventListener("click", function() {
     diceDOM.src = "dice-" + dice + ".png";
 
     // 3. Update the round score IF the rolled number was NOT a 1
-
+    if (dice !== 1) {
+        // Add score
+        roundScore += dice;
+        document.querySelector("#current-" + activePlayer).textContent = roundScore;
+    } else {
+        //Next player
+        nextPlayer();
+    }
 }); //funkcja anonimowa
+
+document.querySelector(".btn-hold").addEventListener("click", function () {
+    // Add the current score to global score
+    scores[activePlayer] += roundScore;
+    // Update the ui
+    document.querySelector("#score-" + activePlayer).textContent = scores[activePlayer];
+    nextPlayer();
+    // Check if player won the game
+});
+
+function nextPlayer () {
+    activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
+    roundScore = 0;
+
+    document.getElementById('current-0').textContent = "0";
+    document.getElementById('current-1').textContent = "0";
+
+    document.querySelector(".player-0-panel").classList.toggle("active");
+    document.querySelector(".player-1-panel").classList.toggle("active");
+
+    document.querySelector(".dice").style.display = "none";
+}
 
 
 //SETTER - bo ustawiamy wartość
